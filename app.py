1c2bda293255cf67619395f9d88ec28126ad69f1
Lee Colsant <lee.colsant@gmail.com>
2018-11-19 21:52:43 -0800

    update app.py

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template("index.html")
-# app.run(debug=True)
-app.run()
+
+if __name__ == '__main__':
+  app.run(debug=True)
+# app.run()
