b36fb2b184f79b288ecf91865d36430ba4a4dad8
Lee Colsant <lee.colsant@gmail.com>
2018-11-19 21:58:10 -0800

    final deployment files

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-if __name__ == '__main__':
+if __name__ == '__main__':   #needed for deployment
   app.run(debug=True)
-# app.run()
+
